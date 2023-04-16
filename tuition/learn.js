3ed15b8019fcc2b8525ae25035e07962cbdb2ea7
Aamir Raza <81976589+Merciful123@users.noreply.github.com>
2023-04-16 18:00:31 +0530

    Add files via upload

---
+++ tuition/learn.js
@@ -0,0 +1,221 @@
+// let num = 8473
+// console.log(num)
+
+// let str = 'Allah'
+
+// console.log(num + str)
+
+// document.write("May Allha forgive us")
+
+// // Array type of data structure
+
+// var g = 5
+// var h =
+// console.log(g%h)
+
+// let array = ["Allah", "Merciful", "Rahman"]
+
+// let array2 = [1, 2, 4, 5]
+
+// console.log(array)
+// console.log(array+array2)
+
+// //  String declaration
+
+// //  String interpolation
+// let myname = 'Aamir'
+
+// console.log(`${myname} Raza`)
+
+// let newNum = 4 ** 2
+
+// console.log(newNum)
+
+// // let secNum = 50 ** 2
+
+// // let addition = newNum +secNum
+// // newNum += 10
+
+// // let Addtion = newNum + secNum
+
+// // secNum += newNum
+
+// let num1 = 2;
+
+// let num2 = 4;
+
+// console.log(num1>num2)
+
+// // let newVar = null
+// // console.log(newVar)
+
+// // console.log(secNum);
+
+// let aamir = "May Allah forgive us"
+// console.log(aamir)
+
+// {
+
+//     let aamir = "May Allah have mercy on us"
+//     console.log(aamir)
+
+// }
+
+//  Assignment operator :- use to assign values
+
+// let a = 3
+// let b = 10
+// let g;
+
+// g = b
+
+// let str = "sldkjs"
+// let str2
+
+// console.log(str = str2)
+
+// let bool1 = false
+
+// let bool2 = true
+
+// console.log(bool1 = bool2)
+
+//
+
+// console.log(g = b)
+
+// console.log( bool1 == bool2)
+
+// if (!condition) {
+//     console.log(a > b);
+//     console.log("Inside");
+
+// }
+
+// console.log("Outside")
+
+// eqaulity operator
+
+// let a = "2"
+
+// let b = 2
+
+// console.log(a==b)
+
+// else
+
+// if (2 == "2") {
+//   console.log("equal");
+// } else if (3 < 5) {
+//   console.log("This is not true");
+// } else {
+//   console.log("not equal");
+// }
+
+// let data = Date.now()
+// console.log(data);
+
+// const time = new Date().getHours();
+
+// console.log(time);
+
+// import os from "os"
+// console.log(os.type())
+
+// const findMedianSortedArrays = (nums1, nums2) => {
+//   const Arr = [...nums1, ...nums2].sort((a, b) => a - b);
+
+//   const lenArr = Arr.length;
+//   if (Arr.length % 2 === 0) {
+//     const mid1 = Arr[lenArr / 2];
+//     const mid2 = Arr[lenArr / 2 - 1];
+//     return (mid1 + mid2) / 2;
+//   } else {
+//     return Arr[Math.floor(lenArr / 2)];
+//   }
+// };
+
+// let image = document.getElementById("pic");
+// var n = "GOd";
+
+// n = "Aamir"
+
+
+// n = "Mercy"
+// console.log(n)
+
+
+
+
+
+  // = 10000;
+// for (i = 0; i < n; i++) {
+//   image.innerHTML =
+//     '<img src= "https://img.freepik.com/free-photo/moon-light-shine-through-window-into-islamic-mosque-interior_1217-2597.jpg?w=900&t=st=1678123542~exp=1678124142~hmac=b6cef3b5ec9e55d625cb846047228491547ff2c714baf0d085a260f30ed2fb83" />';
+// }
+
+// for (i = 0; i <= n; i++) {
+//   console.log(i);
+//   document.write("<p>May Allah Have mercy on us</p>");
+//   // document.getElementById("pic").src =
+//   //   "https://img.freepik.com/free-photo/moon-light-shine-through-window-into-islamic-mosque-interior_1217-2597.jpg?w=900&t=st=1678123542~exp=1678124142~hmac=b6cef3b5ec9e55d625cb846047228491547ff2c714baf0d085a260f30ed2fb83";
+
+//   document.write(
+//     `
+    
+//     </div>
+//     <img
+//     src=
+//        "https://img.freepik.com/free-photo/moon-light-shine-through-window-into-islamic-mosque-interior_1217-2597.jpg?w=900&t=st=1678123542~exp=1678124142~hmac=b6cef3b5ec9e55d625cb846047228491547ff2c714baf0d085a260f30ed2fb83"
+//     />`
+//   );
+
+  // document.write("<p>Hellow</p>");
+// }
+
+
+
+
+//  control flow statement !!!!!!
+const variable1 = '50'; 
+console.log(variable1)
+
+const variable2 = "100";
+
+const variable3 = "1000"; 
+
+let var1 = "3"
+
+let var2 = "3"
+
+let var3 = 3
+
+if (var1 === var2) {
+  console.log(variable1)
+  console.log(variable1)
+}
+
+if (var1 === var3) {
+  console.log(variable2)
+}
+
+else {
+  console.log(variable3)
+}
+
+
+//  for loop
+
+var name = "May Allah forgive us";
+
+let numOfTimes = 5;
+
+for (i = 0; i < numOfTimes; i++) {
+  console.log(name)
+  document.write("<p>Ar Rahman</p>")
+  document.write("<img src='https://images.pexels.com/photos/2236674/pexels-photo-2236674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'>Ar Rahman</img>")
+
+}
+
+
+
